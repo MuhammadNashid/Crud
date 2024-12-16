bdde75eeddc6c685700290dec4c317e846357465
MuhammadNashid <muhammadnashid905@gmail.com>
2024-12-16 16:48:51 +0530

    file added

--- ServerSide/connection.js
+++ ServerSide/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 
 export default async function connection(){
-    const db=await mongoose.connect('mongodb://127.0.0.1:27017/crud');
+    const db=await mongoose.connect('mongodb+srv://vavag77429:SR8a0pYdMlwjAwmd@cluster0.pwmvz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/crud');
     console.log("database connected");
     
     return db
